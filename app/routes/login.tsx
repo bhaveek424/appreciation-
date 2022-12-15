d5745bf5298c2f82c3fad691aec7ec4a6ece4770
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-16 02:34:08 +0530

    Sign Up button and field logic

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,9 +3,12 @@ import { FormField } from "~/components/form-field";
 import { Layout } from "~/components/layout";
 
 export default function Login() {
+  const [action, setAction] = useState("login");
   const [formData, setFormData] = useState({
     email: "",
     password: "",
+    firstName: "",
+    lastName: "",
   });
 
   const handleInputChange = (
@@ -21,11 +24,18 @@ export default function Login() {
   return (
     <Layout>
       <div className="h-full flex justify-center items-center flex-col gap-y-4">
+        <button
+          onClick={() => setAction(action == "login" ? "register" : "login")}
+          className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
+          {action === "login" ? "Sign Up" : "Sign In"}
+        </button>
         <h2 className="text-5xl font-extrabold text-yellow-300">
           Welcome to Appreciation!
         </h2>
         <p className="font-semibold text-slate-300">
-          Log In To Give Some Praise!
+          {action === "login"
+            ? "Log in To Give Some Praise!"
+            : "Sign Up To Get Started!"}
         </p>
 
         <form className="rounded-2xl bg-gray-200 p-6 w-96">
@@ -45,12 +55,28 @@ export default function Login() {
             onChange={(e) => handleInputChange(e, "password")}
             type="password"
           />
+          {action !== "login" ? (
+            <>
+              <FormField
+                htmlFor="firstName"
+                label="First Name"
+                value={formData.firstName}
+                onChange={(e) => handleInputChange(e, "firstName")}
+              />
+              <FormField
+                htmlFor="lastName"
+                label="Last Name"
+                value={formData.lastName}
+                onChange={(e) => handleInputChange(e, "lastName")}
+              />
+            </>
+          ) : null}
 
           {/* Submit */}
           <div className="w-full text-center">
             <input
               type="submit"
-              value="Sign In"
+              value={action === "login" ? "Sign In" : "Sign Up"}
               className="rounded-xl cursor-pointer mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
             />
           </div>
