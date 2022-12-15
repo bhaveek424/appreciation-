e578ca5f339d81f2d7360686090fc40fb34f52e2
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-15 23:46:42 +0530

    remix, tailwind, mongodb, prisma setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Appreciation App
+
+# TODO
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="h-screen w-full bg-slate-600">
+      <h2 className="font-bold text-5xl text-blue-400">TailwindCSS works</h2>
+    </div>
+  );
+}
