e4d78682a9957fe36518d8dbd218a058d7ad1dab
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-17 00:44:12 +0530

    user popping modal

--- app/routes/home/kudo.$userId.tsx
+++ app/routes/home/kudo.$userId.tsx
@@ -1,9 +1,22 @@
+import { json, LoaderFunction, redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { Modal } from "~/components/modal";
+import { getUserById } from "~/utils/users.server";
 
+export const loader: LoaderFunction = async ({ params }) => {
+  const { userId } = params;
+  if (typeof userId !== "string") {
+    return redirect("/home");
+  }
+
+  const user = await getUserById(userId);
+  return json({ user });
+};
 export default function KudoModal() {
+  const { user } = useLoaderData();
   return (
     <Modal isOpen={true}>
-      <h2>This is a modal</h2>
+      <h2>{user.profile.firstName}</h2>
     </Modal>
   );
 }
